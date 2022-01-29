e69a0c534536beb1d8b94d1afe02915a562c55f4
Rhenn Handel <rwhandel@gmail.com>
2022-01-29 18:35:29 -0500

    Creating a command system. Unfinished.

---
+++ WizardFight/Assets/Scripts/Entity.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    protected float lifespan;
+    protected float direction;
+    protected float speed;
+    protected float damage;
+
+    
+}
